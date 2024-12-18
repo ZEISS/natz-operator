8f79fe211718309be147cc1f087e07f9ceee8405
Sebastian Döll <sebastian@katallaxie.dev>
2024-12-18 11:43:00 +0000

    chore: closes :warning: Action Required: Replace Deprecated gcr.io/kubebuilder/kube-rbac-proxy #46

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -14,7 +15,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 )
 
 var (
@@ -29,6 +30,8 @@ type flags struct {
 	enableLeaderElection bool
 	metricsAddr          string
 	probeAddr            string
+	secureMetrics        bool
+	enableHTTP2          bool
 }
 
 var f = &flags{}
@@ -50,6 +53,8 @@ func init() {
 	rootCmd.Flags().BoolVar(&f.enableLeaderElection, "leader-elect", f.enableLeaderElection, "only one controller")
 	rootCmd.Flags().StringVar(&f.metricsAddr, "metrics-bind-address", ":8080", "metrics endpoint")
 	rootCmd.Flags().StringVar(&f.probeAddr, "health-probe-bind-address", ":8081", "health probe")
+	rootCmd.Flags().BoolVar(&f.secureMetrics, "secure-metrics", f.secureMetrics, "serve metrics over https")
+	rootCmd.Flags().BoolVar(&f.enableHTTP2, "enable-http2", f.enableHTTP2, "enable http/2")
 
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
@@ -62,11 +67,31 @@ func run(ctx context.Context) error {
 		Development: true,
 	}
 
+	// if the enable-http2 flag is false (the default), http/2 should be disabled
+	// due to its vulnerabilities. More specifically, disabling http/2 will
+	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
+	// Rapid Reset CVEs. For more information see:
+	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
+	// - https://github.com/advisories/GHSA-4374-p667-p6c8
+	disableHTTP2 := func(c *tls.Config) {
+		setupLog.Info("disabling http/2")
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	tlsOpts := []func(*tls.Config){}
+	if !f.enableHTTP2 {
+		tlsOpts = append(tlsOpts, disableHTTP2)
+	}
+
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		Metrics:                server.Options{BindAddress: f.metricsAddr},
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress:   f.metricsAddr,
+			SecureServing: f.secureMetrics,
+			TLSOpts:       tlsOpts,
+		},
 		HealthProbeBindAddress: f.probeAddr,
 		LeaderElection:         f.enableLeaderElection,
 		LeaderElectionID:       "432c802.zeiss.com",
